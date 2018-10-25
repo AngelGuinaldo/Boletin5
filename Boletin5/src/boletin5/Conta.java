8380ceb6c9cc64a56b2e551a209746ed8217ae02
aguinaldogonzalez <aguinaldogonzalez@nautilus4.danielcastelao.org>
2018-10-25 08:37:41 +0200

    Despues de Factorizar

--- Boletin5/src/boletin5/Conta.java
+++ Boletin5/src/boletin5/Conta.java
@@ -1,69 +1,72 @@
-
 package boletin5;
 
 import java.util.Scanner;
 
-
 public class Conta {
+
     private String nomeCliente;
     private String numeroConta;
     private double saldo;
-    
+
     //constructor vacio
-    public Conta(){
-        
+    public Conta() {
+
     }
 
     //Constructor con parametros
-    public Conta(String nomeCliente,String numeroConta,Double saldo){
+    public Conta(String nomeCliente, String numeroConta, Double saldo) {
         this.saldo = saldo;
-        
-    //setters
+
+        //setters
     }
-    public void setCliente(String cliente){
-        this.nomeCliente=cliente;
+
+    public void setCliente(String cliente) {
+        this.nomeCliente = cliente;
     }
-    public void setConta(String conta){
-        this.numeroConta=conta;
+
+    public void setConta(String conta) {
+        this.numeroConta = conta;
     }
-    public void setSaldo(double saldo){
-        this.saldo=saldo;
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
+
     //getters
-    public String getCliente(String cliente){
+    public String getCliente(String cliente) {
         return cliente;
     }
-    
-    public String getConta(String conta){
+
+    public String getConta(String conta) {
         return conta;
     }
-    
-    public double getSaldo(double saldo){
+
+    public double getSaldo(double saldo) {
         return saldo;
     }
+
     //Metodos
-    public double ingreso(double x){
-        if( x <0 );
+    public double ingreso(double x) {
+        if (x < 0);
         Scanner ler = new Scanner(Sytem.in);
         double ingreso;
         ingreso = x;
         ingreso = ler.nextDouble();
-        return saldo+x;
+        return saldo + x;
     }
-       
-    public double reintegro(double x){
-        while (x<=saldo){
+
+    public double reintegro(double x) {
+        while (x <= saldo) {
             double saldoResultante;
-            saldoResultante = saldo-x;
-        return saldoResultante;
+            saldoResultante = saldo - x;
+            return saldoResultante;
         }
         return 0;
-        
-       
+
     }
-    
-    public void verDatos(){
-        System.out.println("Nome cliente: "+nomeCliente + "; Numero de conta:"+ numeroConta + "; Saldo actual: "+saldo);
+
+    public void verDatos() {
+        System.out.println("Nome cliente: " + nomeCliente + "; Numero de conta:" + numeroConta + "; Saldo actual: " + saldo);
 
     }
 }
