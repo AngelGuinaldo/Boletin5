d20f3813a6dd9d88c3a94081f915b51a0bde321a
aguinaldogonzalez <aguinaldogonzalez@nautilus4.danielcastelao.org>
2018-10-25 08:36:35 +0200

    Antes de Factorizar

---
+++ Boletin5/src/boletin5/Boletin5.java
@@ -0,0 +1,13 @@
+
+package boletin5;
+
+
+public class Boletin5 {
+
+   
+    public static void main(String[] args) {
+       Conta conta= new Conta();
+       
+    }
+    
+}
---
+++ Boletin5/src/boletin5/Conta.java
@@ -0,0 +1,69 @@
+
+package boletin5;
+
+import java.util.Scanner;
+
+
+public class Conta {
+    private String nomeCliente;
+    private String numeroConta;
+    private double saldo;
+    
+    //constructor vacio
+    public Conta(){
+        
+    }
+
+    //Constructor con parametros
+    public Conta(String nomeCliente,String numeroConta,Double saldo){
+        this.saldo = saldo;
+        
+    //setters
+    }
+    public void setCliente(String cliente){
+        this.nomeCliente=cliente;
+    }
+    public void setConta(String conta){
+        this.numeroConta=conta;
+    }
+    public void setSaldo(double saldo){
+        this.saldo=saldo;
+    }
+    //getters
+    public String getCliente(String cliente){
+        return cliente;
+    }
+    
+    public String getConta(String conta){
+        return conta;
+    }
+    
+    public double getSaldo(double saldo){
+        return saldo;
+    }
+    //Metodos
+    public double ingreso(double x){
+        if( x <0 );
+        Scanner ler = new Scanner(Sytem.in);
+        double ingreso;
+        ingreso = x;
+        ingreso = ler.nextDouble();
+        return saldo+x;
+    }
+       
+    public double reintegro(double x){
+        while (x<=saldo){
+            double saldoResultante;
+            saldoResultante = saldo-x;
+        return saldoResultante;
+        }
+        return 0;
+        
+       
+    }
+    
+    public void verDatos(){
+        System.out.println("Nome cliente: "+nomeCliente + "; Numero de conta:"+ numeroConta + "; Saldo actual: "+saldo);
+
+    }
+}
